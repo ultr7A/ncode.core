4a63c2b02efee5090fe95703e0d4a0e29e734922
Jeremy Evans <openspacehexagon@gmail.com>
2022-02-17 19:44:12 -0500

    define enum for primitive data types

---
+++ src/Domain [╍🌐╍🧭╍]/primitive/type.enum.ts
@@ -0,0 +1,12 @@
+export enum DataType {
+    BOOLEAN = "boolean",
+    INT = "int",
+    FLOAT = "float",
+    STRING = "string",
+    HASH = "hash",
+    ARRAY = "array",
+    NULL = "null",
+    CLASS_DEFINITION = "class",
+    CLASSIFIED_OBJECT = "object",
+    FUNCTION = "function"
+}
