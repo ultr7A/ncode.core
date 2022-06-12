55f0b4fbf2edcfcb91a91ab0ab5f60d507f4ab02
Jeremy Evans <openspacehexagon@gmail.com>
2022-06-12 15:57:30 -0400

    update core

--- src/Domain [╍🌐╍🧭╍]/module/module.ts
+++ src/Domain [╍🌐╍🧭╍]/module/module.ts
@@ -7,6 +7,5 @@ import { ModuleExport } from "./module-export.js";
 export class Module<ModuleTopology extends CodeData> {
     Name: string;
     Code: ModuleTopology;
-    Dependencies: string[];
     Exports: ModuleExport[]
 }
