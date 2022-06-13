a4c3ae1985f157da1ef31fece9d8e7cc050b6f8c
Jeremy Evans <openspacehexagon@gmail.com>
2022-06-13 14:08:05 -0400

    Update core

--- src/Domain [╍🌐╍🧭╍]/module/module.ts
+++ src/Domain [╍🌐╍🧭╍]/module/module.ts
@@ -1,6 +1,7 @@
 import { CodeData }        from "../source/source-code.js";
 import { IBlockStatement, IIdentifier } from "../syntax/0_1_0_structure-concept.js";
 import { ModuleExport }    from "./module-export.js";
+import { ModuleImport } from "./module-import.js";
 import { ModuleTopology_Name } from "./module-structure.enum.js";
 
 
@@ -12,6 +13,7 @@ export class Module<ModuleTopology extends CodeData> {
     AST: IBlockStatement;
     Code: ModuleTopology;
     Topology: ModuleTopology_Name;
-    Exports: ModuleExport[]
+    Exports: ModuleExport[];
+    Imports: ModuleImport[];
 }
 
