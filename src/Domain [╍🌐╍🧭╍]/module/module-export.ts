f97d65569193c2e9caadf2f2d396c2f3a91b8dcf
Jeremy Evans <openspacehexagon@gmail.com>
2022-06-13 11:51:18 -0400

    Update core

--- src/Domain [╍🌐╍🧭╍]/module/module-export.ts
+++ src/Domain [╍🌐╍🧭╍]/module/module-export.ts
@@ -1,6 +1,6 @@
-import { DataStructureDeclaration, Expression, IIdentifier } from "../syntax/0_1_0_structure-concept";
-import { NodeName } from "../syntax/0_1_2_2_structure-implementation.enum";
-import { UnParser } from "../system/un-parser";
+import { DataStructureDeclaration, Expression, IIdentifier } from "../syntax/0_1_0_structure-concept.js";
+import { NodeName } from "../syntax/0_1_2_2_structure-implementation.enum.js";
+import { UnParser } from "../system/un-parser.js";
 /**
  * 
  **/
